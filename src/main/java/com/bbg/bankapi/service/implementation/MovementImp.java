7bf17b8c5de589d0fdf6f000349e309b094a2609
milton <miltone92@gamil.com>
2020-02-28 07:54:21 -0600

    currency layer implementation

--- src/main/java/com/bbg/bankapi/service/implementation/MovementImp.java
+++ src/main/java/com/bbg/bankapi/service/implementation/MovementImp.java
@@ -1,5 +1,6 @@
 package com.bbg.bankapi.service.implementation;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +13,33 @@ import com.bbg.bankapi.service.MovementService;
 
 import org.springframework.stereotype.Service;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
 @Service
 public class MovementImp extends MovementService{
 
     private MovementRepo movementRepo;
     private AccountRepo accountRepo;
+
+    
+    // Currency layer
+    public  final String ACCESS_KEY = "936effdd5bdda5e1ed275b7212e30edd";
+    public  final String BASE_URL = "http://api.currencylayer.com/";
+    public  final String ENDPOINT = "live";
+
+    // this object is used for executing requests to the (REST) API
+    CloseableHttpClient httpClient = HttpClients.createDefault();
     
     public MovementImp(MovementRepo movementRepo, AccountRepo accountRepo){
         this.movementRepo = movementRepo;
@@ -62,9 +85,61 @@ public class MovementImp extends MovementService{
         int amount = movement.getAmount();
       
         if(movement.getType().equalsIgnoreCase("transfer")){
-            //update balance
-            origin.setBalance(origin.getBalance() - amount);
-            destination.setBalance(destination.getBalance() + amount);
+            //check for currency conversion
+            if(origin.getCurrency().equalsIgnoreCase(destination.getCurrency()) != true){
+                //Exchange reate is needed
+            
+                // The following line initializes the HttpGet Object with the URL in order to send a request
+                HttpGet get = new HttpGet(BASE_URL + ENDPOINT + "?access_key=" + ACCESS_KEY + "&currencies=USD,CRC&format=1");
+
+                try {
+                    CloseableHttpResponse response =  httpClient.execute(get);
+                    HttpEntity entity = response.getEntity();
+        
+                    // the following line converts the JSON Response to an equivalent Java Object
+                    JSONObject exchangeRates = new JSONObject(EntityUtils.toString(entity));
+                   
+                    //get rate
+                    int rate = (int)exchangeRates.getJSONObject("quotes").getDouble("USDCRC");
+                    System.out.println("1 " + exchangeRates.getString("source") + " in CRC : " + exchangeRates.getJSONObject("quotes").getDouble("USDCRC")); 
+
+                    //Update origin balance before conversion
+                    origin.setBalance(origin.getBalance() - amount);
+                    if(origin.getCurrency().equals("USD") && destination.getCurrency().equals("CRC")){
+                        
+                        //Convert to CRC
+                       amount = amount * rate;
+                       //Update destination balance
+                       destination.setBalance(destination.getBalance() + amount);
+
+    
+                    }else if( origin.getCurrency().equals("CRC") && destination.getCurrency().equals("USD")){
+                        //Convert to USD
+                        amount = amount / rate;
+                        //Update destination balance
+                        destination.setBalance(destination.getBalance() + amount);
+                    }
+    
+                }catch (ClientProtocolException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }   
+    
+            }
+            else{
+                origin.setBalance(origin.getBalance() - amount);
+                destination.setBalance(destination.getBalance() + amount);
+            }
+            ////update balance
             accountRepo.save(destination);
         }else{
             //Case of payments and deposits
